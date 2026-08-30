7a54f44690cc2545c38f14d4c7bedb3a3a3f4c5e
khushghetiya665 <khushghetiya665@gmail.com>
2026-08-30 18:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (31.10%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int first = 0, last = nums.length - 1;
+
+        while (first <= last) {
+            int mid = (first + last) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (nums[mid] < target) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
