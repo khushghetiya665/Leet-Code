1a5024b3943a9b167b5bb2eda46899d3d96828f3
khushghetiya665 <khushghetiya665@gmail.com>
2026-08-30 19:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (31.44%) - LeetSync

---
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int mid = nums.length / 2;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[mid] == nums[i] && i != mid) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
